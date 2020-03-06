da499c7632269573ed91fbc4c06220e0bfafefa5
pbca26 <pbca26@gmail.com>
2020-03-06 18:44:08 +0300

    validate address helper

---
+++ src/lib/validate-address.js
@@ -0,0 +1,20 @@
+import {KOMODO} from './../constants';
+import bitcoin from 'bitcoinjs-lib';
+
+const checkPublicAddress = (address) => {
+  try {
+    const b58check = bitcoin.address.fromBase58Check(address);
+
+    if (b58check.version === KOMODO.pubKeyHash ||
+        b58check.version === KOMODO.scriptHash) {
+      return true;
+    }
+
+    return false;
+  } catch (e) {
+    // TODO: throw error
+    return 'Invalid pub address';
+  }
+};
+
+export default checkPublicAddress;
