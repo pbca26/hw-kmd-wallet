341f1e56f6f6e3e1fa483753d901b5b45e2fcfd0
pbca26 <pbca26@gmail.com>
2020-03-20 15:10:50 +0300

    warn browser comp

---
+++ src/WarnBrowser.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {isChrome} from 'react-device-detect';
+import Modal from './Modal';
+
+class WarnBrowser extends React.Component {
+  state = {
+    isChrome: false
+  };
+
+  componentDidMount() {
+    this.setState({isChrome,});
+  }
+
+  close() {
+    this.setState({
+      isChrome: true,
+    });
+  }
+
+  render() {
+    return (
+      <Modal
+        title="Warning: This Browser Is Not Supported"
+        show={this.state.isChrome === false}
+        handleClose={() => this.close()}
+        isCloseable={true}>
+        <p>You are using an unsupported browser.</p>
+        <p>For better compatibility please use Chrome.</p>
+      </Modal>
+    );
+  }
+}
+
+export default WarnBrowser;
