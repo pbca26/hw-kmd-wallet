5eb829b632dba7fbb69c1205e331290430c5c418
pbca26 <pbca26@gmail.com>
2020-03-24 17:44:46 +0300

    add string utils

---
+++ src/lib/string.js
@@ -0,0 +1,28 @@
+const secondsToString = (seconds, skipMultiply, showSeconds) => {
+  const a = new Date(seconds * (skipMultiply ? 1 : 1000));
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  const year = a.getFullYear();
+  const month = months[a.getMonth()];
+  const date = a.getDate();
+  const hour = a.getHours() < 10 ? `0${a.getHours()}` : a.getHours();
+  const min = a.getMinutes() < 10 ? `0${a.getMinutes()}` : a.getMinutes();
+  const sec = a.getSeconds();
+  const time = `${date} ${month} ${year} ${hour}:${min}${(showSeconds ? `:${sec}` : '')}`;
+
+  return time;
+};
+
+module.exports = secondsToString;
