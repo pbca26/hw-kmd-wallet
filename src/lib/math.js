90b0b9c5decf310f3eb5699937c0ef8064d3e7f6
pbca26 <pbca26@gmail.com>
2020-03-24 14:14:12 +0300

    math util remove duplicate code

--- src/lib/math.js
+++ src/lib/math.js
@@ -13,21 +13,6 @@ const convertExponentialToDecimal = (exponentialNumber) => {
   }
 }
 
-const convertExponentialToDecimal = (exponentialNumber) => {
-  // sanity check - is it exponential number
-  const str = exponentialNumber.toString();
-  if (str.indexOf('e') !== -1) {
-    const exponent = parseInt(str.split('-')[1], 10);
-    // Unfortunately I can not return 1e-8 as 0.00000001, because even if I call parseFloat() on it,
-    // it will still return the exponential representation
-    // So I have to use .toFixed()
-    const result = exponentialNumber.toFixed(exponent);
-    return result;
-  } else {
-    return exponentialNumber;
-  }
-}
-
 const fromSats = value => convertExponentialToDecimal(Number(Number(value * 0.00000001).toFixed(8)));
 
 const toSats = value => Number((Number(value).toFixed(8) * 100000000).toFixed(0));
