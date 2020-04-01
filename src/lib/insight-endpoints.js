91cc780e0158c024b1caf734261070f2fe6f9f19
pbca26 <pbca26@gmail.com>
2020-04-01 14:16:44 +0300

    add MORTY and VRSC explorer api endpoints

---
+++ src/lib/insight-endpoints.js
@@ -0,0 +1,7 @@
+const apiEndpoints = {
+  RICK: ['https://rick.kmd.dev/insight-api-komodo/', 'https://rick.explorer.dexstats.info/insight-api-komodo/'],
+  MORTY: ['https://morty.kmd.dev/insight-api-komodo/', 'https://morty.explorer.dexstats.info/insight-api-komodo/'],
+  VRSC: ['https://explorer.komodoplatform.com:10000/vrsc/api/', 'https://vrsc.explorer.dexstats.info/insight-api-komodo/']
+};
+
+export default apiEndpoints;
