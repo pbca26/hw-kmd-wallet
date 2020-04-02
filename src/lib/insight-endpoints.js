f46597062e10d19d8c875ddd71190ffd560814a2
pbca26 <pbca26@gmail.com>
2020-04-02 12:44:22 +0300

    extend insight api end points list

--- src/lib/insight-endpoints.js
+++ src/lib/insight-endpoints.js
@@ -1,7 +1,33 @@
 const apiEndpoints = {
+  AXO: ['https://axo.explorer.dexstats.info/insight-api-komodo/'],
+  ETOMIC: ['https://etomic.explorer.dexstats.info/insight-api-komodo/'],
+  KOIN: ['https://koin.explorer.dexstats.info/insight-api-komodo/'],
+  MESH: ['https://mesh.explorer.dexstats.info/insight-api-komodo/'],
+  JUMBLR: ['https://jumblr.explorer.dexstats.info/insight-api-komodo/'],
+  BET: ['https://bet.explorer.dexstats.info/insight-api-komodo/'],
+  CRYPTO: ['https://crypto.explorer.dexstats.info/insight-api-komodo/'],
+  HODL: ['https://hodl.explorer.dexstats.info/insight-api-komodo/'],
+  ILN: ['https://explorer.ilien.io/insight-api-komodo/'],
+  EQL: ['https://eql.explorer.dexstats.info/insight-api-komodo/'],
+  BTCH: ['https://btch.explorer.dexstats.info/insight-api-komodo/'],
+  DP: ['https://dp.explorer.dexstats.info/insight-api-komodo/'],
+  HUSH3: ['https://hush3.explorer.dexstats.info/insight-api-komodo/'],
+  PIZZA: ['https://pizza.explorer.dexstats.info/insight-api-komodo/'],
+  BEER: ['https://beer.explorer.dexstats.info/insight-api-komodo/'],
+  NINJA: ['https://ninja.explorer.dexstats.info/insight-api-komodo/'],
+  ZEXO: ['http://zex.explorer.dexstats.info/insight-api-komodo/'],
+  KSB: ['http://ksb.explorer.dexstats.info/insight-api-komodo/'],
+  OUR: ['http://our.explorer.dexstats.info/insight-api-komodo/'],
+  MCL:  ['http://mcl.explorer.dexstats.info/insight-api-komodo/'],
+  RFOX: ['https://rfox.explorer.dexstats.info/insight-api-komodo/'],
+  LABS: ['http://labs.explorer.dexstats.info/insight-api-komodo/'],
   RICK: ['https://rick.kmd.dev/insight-api-komodo/', 'https://rick.explorer.dexstats.info/insight-api-komodo/'],
   MORTY: ['https://morty.kmd.dev/insight-api-komodo/', 'https://morty.explorer.dexstats.info/insight-api-komodo/'],
   VRSC: ['https://explorer.komodoplatform.com:10000/vrsc/api/', 'https://vrsc.explorer.dexstats.info/insight-api-komodo/']
+  /* coins below need special handling due to no overwinter support
+  ZILLA: ['https://zilla.explorer.dexstats.info/insight-api-komodo/'],
+  OOT: ['https://oot.explorer.dexstats.info/insight-api-komodo/'],
+  */
 };
 
 export default apiEndpoints;
