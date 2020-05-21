dd80ee5266bd2ae0894d9626c7dd85afec8a753e
pbca26 <pbca26@gmail.com>
2020-05-21 14:24:14 +0300

    refactor lib/explorers as coins obj

---
+++ src/lib/coins.js
@@ -0,0 +1,138 @@
+import {sortObject} from './sort';
+
+const coins = {
+  KMD: {
+    explorer: 'https://kmd.explorer.dexstats.info/',
+  },
+  AXO: {
+    explorer: 'https://axo.explorer.dexstats.info/',
+  },
+  ETOMIC: {
+    explorer: 'https://etomic.explorer.dexstats.info/',
+  },
+  KOIN: {
+    explorer: 'https://koin.explorer.dexstats.info/',
+  },
+  MESH: {
+    explorer: 'https://mesh.explorer.dexstats.info/',
+  },
+  DEX: {
+    explorer: 'https://dex.explorer.dexstats.info/',
+  },
+  SUPERNET: {
+    explorer: 'https://supernet.explorer.dexstats.info/',
+  },
+  DION: {
+    explorer: 'https://explorer.dionpay.com/',
+  },
+  CCL: {
+    explorer: 'https://ccl.explorer.dexstats.info/',
+  },
+  KV: {
+    explorer: 'https://kv.explorer.dexstats.info/',
+  },
+  CHAIN: {
+    explorer: 'https://chain.explorer.dexstats.info/',
+  },
+  PGT: {
+    explorer: 'https://pgt.explorer.dexstats.info/',
+  },
+  MSHARK: {
+    explorer: 'https://mshark.explorer.dexstats.info/',
+  },
+  REVS: {
+    explorer: 'https://revs.explorer.dexstats.info/',
+  },
+  PANGEA: {
+    explorer: 'https://pangea.explorer.dexstats.info/',
+  },
+  JUMBLR: {
+    explorer: 'https://jumblr.explorer.dexstats.info/',
+  },
+  BET: {
+    explorer: 'https://bet.explorer.dexstats.info/',
+  },
+  CRYPTO: {
+    explorer: 'https://crypto.explorer.dexstats.info/',
+  },
+  HODL: {
+    explorer: 'https://hodl.explorer.dexstats.info/',
+  },
+  ILN: {
+    explorer: 'https://explorer.ilien.io/',
+  },
+  BOTS: {
+    explorer: 'https://bots.explorer.dexstats.info/',
+  },
+  MGW: {
+    explorer: 'https://mgw.explorer.dexstats.info/',
+  },
+  WLC21: {
+    explorer: 'https://wlc21.explorer.dexstats.info/',
+  },
+  COQUICASH: {
+    explorer: 'https://coquicash.explorer.dexstats.info/',
+  },
+  BTCH: {
+    explorer: 'https://btch.explorer.dexstats.info/',
+  },
+  HUSH3: {
+    explorer: 'https://hush3.explorer.dexstats.info/',
+  },
+  NINJA: {
+    explorer: 'https://ninja.explorer.dexstats.info/',
+  },
+  SEC: {
+    explorer: 'https://sec.explorer.dexstats.info/',
+  },
+  THC: {
+    explorer: 'https://thc.explorer.dexstats.info/',
+  },
+  KMDICE: {
+    explorer: 'https://kmdice.explorer.dexstats.info/',
+  },
+  ZEXO: {
+    explorer: 'https://zex.explorer.dexstats.info/',
+  },
+  KSB: {
+    explorer: 'https://ksb.explorer.dexstats.info/',
+  },
+  OUR: {
+    explorer: 'https://our.explorer.dexstats.info/',
+  },
+  MCL: {
+    explorer: 'https://mcl.explorer.dexstats.info/',
+  },
+  RFOX: {
+    explorer: 'https://rfox.explorer.dexstats.info/',
+  },
+  LABS: {
+    'https://labs.explorer.dexstats.info/',
+  },
+  VOTE2020: {
+    explorer: 'https://vote2020.explorer.dexstats.info/',
+  },
+  RICK: {
+    explorer: 'https://rick.kmd.dev/',
+  },
+  MORTY: {
+    explorer: 'https://morty.kmd.dev/',
+  },
+  VRSC: {
+    explorer: 'https://vrsc.explorer.dexstats.info/',
+  },
+  ZILLA: {
+    explorer: 'https://zilla.explorer.dexstats.info/',
+  },
+  OOT: {
+    explorer: 'https://oot.explorer.dexstats.info/',
+  },
+  // DP: '',
+  // SHARK: '',
+  // EQL: '',
+  // PIZZA: '',
+  // BEER: '',
+  // DSEC: '',
+};
+
+export default sortObject(coins);
