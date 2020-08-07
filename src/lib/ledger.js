b4f7197761b2ec1c880279eb502602aeab70d70b
pbca26 <pbca26@gmail.com>
2020-08-07 14:15:03 +0300

    change ledger derivation path to 141 (kmd) during device conn test

--- src/lib/ledger.js
+++ src/lib/ledger.js
@@ -49,7 +49,7 @@ const getDevice = async () => {
 const isAvailable = async () => {
   const ledger = await getDevice();
   try {
-    await ledger.getWalletPublicKey(`m/44'/0'/0'/0/0`);
+    await ledger.getWalletPublicKey(`m/44'/141'/0'/0/0`);
     await ledger.close();
     return true;
   } catch (error) {
