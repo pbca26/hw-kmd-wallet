eb131ed7eec2df932b1f7f2a72b37e87bf38152e
pbca26 <pbca26@gmail.com>
2020-08-28 17:36:22 +0300

    main create window enable node integration flag

--- electron/main.js
+++ electron/main.js
@@ -37,13 +37,14 @@ let mainWindow;
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
-
-  // and load the index.html of the app.
-  mainWindow.loadFile("index.html");
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nativeWindowOpen: true, // <-- important for trezor
+      nodeIntegration: true,
+    },
+  });
 
   // important: allow connect popup to open external links in default browser (wiki, wallet, bridge download...)
   mainWindow.webContents.on('new-window', (event, url, frameName, disposition, options, additionalFeatures) => {
@@ -85,18 +86,18 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on("window-all-closed", function() {
+app.on('window-all-closed', function() {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on("activate", function() {
+app.on('activate', function() {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
@@ -104,6 +105,8 @@ app.on("activate", function() {
   }
 });
 
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
 app.on('browser-window-focus', (event, win) => {
   if (!win.isDevToolsOpened()) {
       win.openDevTools();
