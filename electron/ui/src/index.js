617cfdac316a6762110d44da5eb240ef45800275
pbca26 <pbca26@gmail.com>
2020-09-28 17:10:06 +0300

    remove hw-kmd-reward-claim ui

--- electron/ui/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '@fortawesome/fontawesome-free/css/all.css';
-import './index.scss';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
--- electron/ui/src/lib/human-readable-satoshis.js
+++
@@ -1,6 +0,0 @@
-import {toBitcoin} from 'satoshi-bitcoin';
-import scientificToDecimal from 'scientific-to-decimal';
-
-const humanReadableSatoshis = satoshis => scientificToDecimal(toBitcoin(satoshis));
-
-export default humanReadableSatoshis;
