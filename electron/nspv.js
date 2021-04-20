1697382e5549d07a55c43050413478038ce2a9de
pbca26 <pbca26@gmail.com>
2021-04-20 20:51:48 +0300

    (electron) nspv cache helpers

--- electron/nspv.js
+++ electron/nspv.js
@@ -18,6 +18,51 @@ const setMainWindow = (_mainWindow) => {
   mainWindow = _mainWindow;
 };
 
+
+(async function() {
+  cache = await cacheUtil.loadLocalCache('nspv');
+  
+  /*const nspv = startNSPVDaemon('rick');
+  nspvProcesses['rick'] = {
+    process: nspv,
+    pid: nspv.pid,
+  };
+  /*setTimeout(() => {
+    syncCoinData();
+  }, 7000);*/
+})();
+
+/*setInterval(() => {
+  syncCoinData();
+}, NSPV_RECHECK_INTERVAL_TIMEOUT);*/
+
+const getCache = (coin, hash) => {
+  if (cache[coin] && cache[coin][hash]) {
+    console.log(`nspv load from cache ${coin} ${hash}`);
+    return cache[coin][hash].res;
+  }
+
+  return false;
+};
+
+const writeCache = (coin, hash, data) => {
+  if (!cache[coin]) {
+    cache[coin] = {};
+  }
+
+  if (!cache[coin][hash]) {
+    cache[coin][hash] = {
+      req: '',
+      res: '',
+    };
+  }
+
+  cache[coin][hash].req = data.req;
+  cache[coin][hash].res = data.res;
+  console.log(`save to cache ${coin} ${hash}`);
+  cacheUtil.saveLocalCache(cache, 'nspv');
+};
+
 module.exports = {
   setMainWindow,
 };
