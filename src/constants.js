01780eaf2045b9aa1562abaa4d6f7e7b951582e9
pbca26 <pbca26@gmail.com>
2021-05-07 21:01:35 +0300

    const add settings options

--- src/constants.js
+++ src/constants.js
@@ -53,3 +53,21 @@ export const LEDGER_DEVICE_HEX_ENUM = {
   '31100004': 's',
   '33000004': 'x',
 };
+export const SETTINGS = {
+  DISCOVERY_GAP_LIMIT: {
+    10: 10,
+    15: 15,
+    20: '20 (default)',
+    25: 25,
+    30: 30,
+    35: 35,
+    40: 40,
+    45: 45,
+    50: 50,
+  },
+  DISCOVERY_ADDRESS_CONCURRENCY: {
+    2: 2,
+    5: 5,
+    10: '10 (default)',
+  },
+};
